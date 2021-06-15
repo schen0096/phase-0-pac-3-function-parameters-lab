1a256d4d060f1df999d8c5f8515fe39edbf90ae3
steven chen <schen0096@gmail.com>
2021-06-15 02:08:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 // }
 
 function introduction(name) {
-    console.log(`"Hi, my name is ${name}."`);
+    return (`Hi, my name is ${name}.`);
 }
 
 introduction ("Josh");
 introduction ("Nancy");
 
 function introductionWithLanguage(name, language) {
-    console.log(`"Hi, my name is ${name} and I am learning to program in ${language}."`)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 
 introductionWithLanguage("Josh", "Ember.js");
 introductionWithLanguage("Nancy","React")
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    console.log(`"Hi, my name is ${name} and I am learning to program in ${language}."`)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 
 introductionWithLanguageOptional("Gracie");
 
 function introductionWithLanguageOptional2(name, language = "Python") {
-    console.log(`"Hi, my name is ${name} and I am learning to program in ${language}."`)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 
 introductionWithLanguageOptional2("Gracie");
